60cf2164effe76dc766594a4a7bcf31591f37650
ducnd58233 <64019100+ducnd58233@users.noreply.github.com>
2024-06-16 16:09:24 +0700

    feat(M01EX02): solution for homework 8/6/2024 (#4)

---
+++ module-1/08_06_2024_M01EX02/multi_choice_ex6.py
@@ -0,0 +1,14 @@
+def my_func(data, ma, mi):
+    res = []
+    
+    for i in data:
+        if i < mi:
+            res.append(mi)
+        elif i > ma:
+            res.append(ma)
+        else:
+            res.append(i)
+    return res
+
+if __name__ == '__main__':
+    print(my_func([10, 2, 5, 0, 1], 2, 1))
